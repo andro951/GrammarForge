acf930bceecc719e1048bc65e137eabe500cca54
Isaac Richards <andro951@gmail.com>
2026-02-16 20:15:58 -0500

    Added ScriptActionParameter

--- scriptForge/scriptAction.js
+++ scriptForge/scriptAction.js
@@ -7,14 +7,36 @@ ScriptForge.ScriptAction = class ScriptAction {
         this.canCallAction = canCallAction;
         this.description = description;
         this.parameters = parameters;
+        if (parameters != null) {
+            for (let parameter of parameters) {
+                if (!(parameter instanceof ScriptForge.ScriptActionParameter)) {
+                    throw new Error(`Error when trying to create ScriptAction ${name}: All parameters must be instances of ScriptActionParameter. Invalid parameter: ${parameter}`);
+                }
+            }
+        }
     }
-//TODO: needs to have a way to validate the inputs before running the action.
-//Have a condition to say if the action is currently allowed to be run.
-//Then change to tryRun with an output of why it didn't work like an enum
     run = (...args) => {
         if (!this.canCallAction())
             return;
         
+        if (this.parameters != null) {
+            if (args.length != this.parameters.length) {
+                throw new Error(`Error when trying to call ${this.name}: Expected ${this.parameters.length} arguments, but got ${args.length}.  arguments: ${args}`);
+            }
+        }
+
         this.action(...args);
+    }
+}
+
+ScriptForge.ScriptActionParameter = class ScriptActionParameter {
+    constructor(name, description, optional = false) {
+        this.name = name;
+        this.description = description;
+        this.optional = optional;
+    }
+
+    toString = () => {
+        return `${this.name}${this.optional ? " (optional)" : ""}: ${this.description}`;
     }
 }
