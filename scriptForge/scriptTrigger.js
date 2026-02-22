8c85ac4cfcfeec15bace94293a551e4d424c1572
Isaac Richards <andro951@gmail.com>
2026-02-22 14:17:11 -0500

    Added ScriptParameter

--- scriptForge/scriptTrigger.js
+++ scriptForge/scriptTrigger.js
@@ -1,5 +1,13 @@
 "use strict";
 
+ScriptForge.ScriptParameter = class ScriptParameter {
+    constructor(internalName, scriptName, description) {
+        this.internalName = internalName;
+        this.scriptName = scriptName;
+        this.description = description;
+    }
+}
+
 ScriptForge.ScriptTrigger = class ScriptTrigger {
     constructor(name, description, parameters, scriptForge) {
         this.name = name;
@@ -12,8 +20,8 @@ ScriptForge.ScriptTrigger = class ScriptTrigger {
             throw new Error('Trigger description must be a string');
 
         this.parameters = parameters;
-        if (typeof parameters !== 'map')
-            throw new Error('Trigger parameters must be a map');
+        if (!Array.isArray(parameters) || !parameters.every(p => p instanceof ScriptForge.ScriptParameter))
+            throw new Error('Trigger parameters must be an array of ScriptParameter instances');
 
         this.scriptForge = scriptForge;
         if (!(scriptForge instanceof ScriptForge))
@@ -29,7 +37,7 @@ ScriptForge.ScriptTrigger = class ScriptTrigger {
         return (typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean');
     }
     run = (args = null) => {
-        argsMap.clear();
+        ScriptForge.ScriptTrigger.argsMap.clear();
 
         if (args !== null) {
             if (typeof args !== 'function')
@@ -39,15 +47,15 @@ ScriptForge.ScriptTrigger = class ScriptTrigger {
             if (!(argsResult instanceof Map))
                 throw new Error('args function must return a map or null');
 
-            if (argsResult.size !== this.parameters.size)
-                throw new Error(`Expected ${this.parameters.size} arguments but got ${argsResult.size}`);
+            if (argsResult.size !== this.parameters.length)
+                throw new Error(`Expected ${this.parameters.length} arguments but got ${argsResult.size}`);
 
             for (const [key, value] of argsResult.entries()) {
                 if (value === null || value === undefined)
                     throw new Error(`Value for key ${key} cannot be null or undefined`);
 
                 if (ScriptForge.ScriptTrigger.isSimpleType(value)) {
-                    argsMap.set(key, value);
+                    ScriptForge.ScriptTrigger.argsMap.set(key, value);
                 }
                 else {
                     const clonedValue = value.clone;
@@ -57,13 +65,13 @@ ScriptForge.ScriptTrigger = class ScriptTrigger {
                     if (clonedValue === value)
                         throw new Error(`Value for key ${key} clone getter must return a different instance`);
 
-                    argsMap.set(key, clonedValue);
+                    ScriptForge.ScriptTrigger.argsMap.set(key, clonedValue);
                 }
             }
         }
         
         for (const script of this.registeredScripts) {
-            this.scriptForge.gf.exec(script.ast, argsMap, this.scriptForge.allGettersFunctions);
+            this.scriptForge.gf.exec(script.ast, ScriptForge.ScriptTrigger.argsMap, this.scriptForge.allGettersFunctions);
         }
     }
 }
