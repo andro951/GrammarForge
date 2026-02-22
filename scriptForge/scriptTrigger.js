57d6e1ae2866d72a4034958f4367ad797f756e99
Isaac Richards <andro951@gmail.com>
2026-02-22 13:45:04 -0500

    Added args to ScriptTrigger

--- scriptForge/scriptTrigger.js
+++ scriptForge/scriptTrigger.js
@@ -10,10 +10,43 @@ ScriptForge.ScriptTrigger = class ScriptTrigger {
     registerScript = (script) => {
         this.registeredScripts.push(script);
     }
-    run = () => {
+    static argsMap = new Map();
+    static isSimpleType = (value) => {
+        return (typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean');
+    }
+    run = (args = null) => {
+        argsMap.clear();
+
+        if (args !== null) {
+            if (typeof args !== 'function')
+                throw new Error('args must be a function that returns a map or null');
+
+            const argsResult = args();
+            if (!(argsResult instanceof Map))
+                throw new Error('args function must return a map or null');
+
+            for (const [key, value] of argsResult.entries()) {
+                if (value === null || value === undefined)
+                    throw new Error(`Value for key ${key} cannot be null or undefined`);
+
+                if (ScriptForge.ScriptTrigger.isSimpleType(value)) {
+                    argsMap.set(key, value);
+                }
+                else {
+                    const clonedValue = value.clone;
+                    if (clonedValue === undefined)
+                        throw new Error(`Value for key ${key} must be a simple type or have a clone getter`);
+
+                    if (clonedValue === value)
+                        throw new Error(`Value for key ${key} clone getter must return a different instance`);
+
+                    argsMap.set(key, clonedValue);
+                }
+            }
+        }
+        
         for (const script of this.registeredScripts) {
-            //TODO: possibly pass the static variables in instead of null, then update after?
-            ZonScript.gf.exec(script.ast, null, this.scriptForge.allGettersFunctions);
+            ZonScript.gf.exec(script.ast, argsMap, this.scriptForge.allGettersFunctions);
         }
     }
 }
